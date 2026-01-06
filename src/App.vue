a12acf48f6ccd3ea8d2b2f88b606f1d24cc63d30
BraianGerez7733 <85425329+BraianGerez7733@users.noreply.github.com>
2026-01-06 03:01:30 +0000

    regresando al modelo anterior

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,21 @@
 </script>
 
 <template>
-  <main class="min-h-screen flex flex-col items-center px-6 text-center pt-8 bg-gray-100">
+<main class="min-h-screen flex flex-col items-center px-6 text-center pt-8">
 
-    <h1 class="text-5xl md:text-6xl font-bold text-red-600 mb-1 underline">
-      Jesús
-    </h1>
+  <h1 class="text-5xl md:text-6xl font-bold text-red-600 mb-1">
+  Jesús
+</h1>
 
-    <h2 class="text-4xl md:text-5xl font-semibold text-slate-800 mb-2">
-      La Biblia · RVR 1960
-    </h2>
+<h2 class="text-4xl md:text-5xl font-semibold text-slate-800 mb-6">
+  La Biblia · RVR 1960
+</h2>
 
-    <p class="text-sm text-slate-500 mb-6">
-      🔴 Prueba de deploy – debería verse este texto
-    </p>
 
     <p class="text-lg text-slate-600 max-w-2xl mb-8">
       Un espacio claro y accesible para leer, estudiar y meditar en las Escrituras,
       centrado en Jesucristo.
     </p>
-
     <div class="flex gap-4"> 
       <button class="px-6 py-3 rounded-xl bg-slate-800 text-white hover:bg-slate-700 transition">
         Comenzar lectura
@@ -28,9 +24,9 @@
       <button class="px-6 py-3 rounded-xl border border-slate-300 text-slate-700 hover:bg-slate-100 transition">
         Explorar evangelios
       </button>
-      <button class="px-6 py-3 rounded-xl bg-slate-800 text-white hover:bg-slate-700 transition">
-        Diferencias con otras religiones
-      </button>
+     <button class="px-6 py-3 rounded-xl bg-slate-800 text-white hover:bg-slate-700 transition">
+  Diferencias con otras religiones
+</button>
     </div>
   </main>
 </template>
