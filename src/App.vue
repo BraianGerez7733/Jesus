540ca1d43b9ffe93ac5fa0b72dae8d9a06dc37d2
BraianGerez7733 <85425329+BraianGerez7733@users.noreply.github.com>
2026-01-07 13:40:16 +0000

    app corregido eliminamos template

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,3 @@
-<script setup>
-</script>
-
-<template>
-<main class="min-h-screen flex flex-col items-center justify-center px-6 text-center">
-
-  <h1 class="text-5xl md:text-6xl font-bold text-slate-600 mb-1">
-  Jesús
-</h1>
-
-<h2 class="text-4xl md:text-5xl font-semibold text-slate-800 mb-6">
-  La Biblia · RVR 1960
-</h2>
-
-
-    <p class="text-lg text-slate-600 max-w-2xl mb-8">
-      Un espacio claro y accesible para leer, estudiar y meditar en las Escrituras,
-      centrado en Jesucristo.
-    </p>
-    <div class="flex gap-4"> 
-      <button class="px-6 py-3 rounded-xl bg-slate-800 text-white hover:bg-slate-700 transition">
-        Comenzar lectura
-      </button>
-      <button class="px-6 py-3 rounded-xl border border-slate-300 text-slate-700 hover:bg-slate-100 transition">
-        Explorar evangelios
-      </button>
-     <button class="px-6 py-3 rounded-xl bg-slate-800 text-white hover:bg-slate-700 transition">
-  Diferencias con otras religiones
-</button>
-    </div>
-  </main>
-</template>
 <template>
   <router-view />
 </template>
